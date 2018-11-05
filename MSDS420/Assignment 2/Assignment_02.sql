3258ab5ad602afec52b980862d5e4f599d79aa9d
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-11-05 08:33:51 -0500

    MSDS 422, assignment 02
    MSDS 422, assignment 02

--- MSDS420/Assignment 2/Assignment_02.sql
+++ MSDS420/Assignment 2/Assignment_02.sql
@@ -9,19 +9,29 @@ SELECT COUNT(*) AS [Q2] FROM CUSTOMER WHERE [CUS_BALANCE] > 500;
 -- 3.) Generate a listing of all purchases made by customers.
 
 SELECT 
-    C.[CUS_FNAME] || ' ' || C.[CUS_LNAME] AS [Customer Name],
-    COUNT( [ALL | DISTINCT] ) AS [Orders]
+   C.[CUS_CODE] AS [CustomerCode],
+   C.[CUS_FNAME] || ' ' || C.[CUS_LNAME] AS [Customer Name],
+   I.[INV_NUMBER] AS [Invoice],
+   I.[INV_DATE] AS [InvoiceDate],
+   L.[LINE_NUMBER] AS [LineNumber],
+   P.[P_CODE] AS [ProductCode],
+   P.[P_DESCRIPT] AS [ProductDescription]
 FROM 
-    [INVOICE] I
-    INNER JOIN CUSTOMER C ON 
-        I.[CUS_CODE] = C.[CUS_CODE]
-GROUP BY
-    I.[CUS_CODE];
-    
--- 4.) Generate a listing of customer purchases, including the subtotals for each of the invoice line numbers.
+    [PRODUCT] P
+    INNER JOIN [LINE] L
+        ON  P.[P_CODE] = L.[P_CODE]
+   INNER JOIN [INVOICE] I
+       ON I.[INV_NUMBER] = L.[INV_NUMBER]
+   INNER JOIN [CUSTOMER] C
+       ON I.[CUS_CODE] = C.[CUS_CODE]
+;
+       
+-- 4.) Generate a listing of customer purchases, 
+-- including the subtotals for each of the invoice line numbers.
 
 SELECT
     C.[CUS_FNAME] || ' ' || C.[CUS_LNAME] AS [Customer Name],
+    I.[INV_NUMBER] AS [Invoice],
     SUM( L.[LINE_PRICE] * L.[LINE_UNITS] ) AS [Balance]
 FROM 
     INVOICE I
@@ -33,20 +43,25 @@ GROUP BY
     I.[INV_NUMBER]
 ;
  
--- 5.) List the balance characteristics of the customers who have made purchases during the current invoice cycle—that is, for the customers who appear in the INVOICE table.
+-- 5.) List the balance characteristics of the customers who have made
+--  purchases during the current invoice cycle—that is, for the customers who 
+-- appear in the INVOICE table.
 
 SELECT 
-    I.[CUS_CODE] AS [CustomerId],
     C.[CUS_FNAME] || ' ' || C.[CUS_LNAME] AS [Customer Name],
-    COUNT( [ALL | DISTINCT] ) AS [Orders],
-    SUM( C.[CUS_BALANCE] ) AS [Balance]
+    MAX( C.[CUS_BALANCE] ) AS [Current Balance],
+    ROUND( SUM( L.[LINE_UNITS] * L.[LINE_PRICE] ), 2) AS [Outstanding],
+    ROUND( MAX( C.[CUS_BALANCE] ) + SUM( L.[LINE_UNITS] * L.[LINE_PRICE] ), 2 ) AS [Projected Balance]
 FROM 
     [INVOICE] I
     INNER JOIN CUSTOMER C ON 
         I.[CUS_CODE] = C.[CUS_CODE]
+    INNER JOIN [LINE] L
+        ON I.[INV_NUMBER] = L.[INV_NUMBER]
 GROUP BY
-    I.[CUS_CODE];
-    
+    C.[CUS_CODE]
+;
+
 -- 6.) Find a listing of customers who did not make purchases during the invoicing period.
 
 SELECT
@@ -64,6 +79,8 @@ WHERE
 SELECT
     [P_DESCRIPT] AS [Product],
     [P_QOH] * [P_PRICE] AS [ValueOnHand],
-    ( CASE [P_DISCOUNT] WHEN 0 THEN 1 ELSE 1 - [P_DISCOUNT] END ) * [P_PRICE] * [P_QOH] AS [DiscountedValue]
+    ( CASE [P_DISCOUNT] WHEN 0 THEN 1 ELSE 1 - [P_DISCOUNT] END ) * 
+        [P_PRICE] * [P_QOH] AS [DiscountedValue]
 FROM
     [PRODUCT]
+;
